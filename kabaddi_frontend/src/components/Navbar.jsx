71e5ddb664b587c6d1a2641de1f6a214cb7f5f16
Anush-ui <anush1320@gmail.com>
2026-03-22 17:28:07 +0530

    Updated frontend files

---
+++ kabaddi_frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+        <nav className='nav'>
+            <ul className="nav-list">
+                <li>KABADDI ANALYTICS</li>
+                <li>Dashboard</li>
+                <li>Players</li>
+                <li>Matches</li>
+                <li>Reports</li>
+                
+            </ul>
+        </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
